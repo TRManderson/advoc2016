465508f55f3443563a4f80d64c2cb6c3eb20250d
Tom Manderson <me@trm.io>
2016-12-10 19:45:21 +1000

    Added day 4 pt 2

--- src/Day4/Main.hs
+++ src/Day4/Main.hs
@@ -6,7 +6,7 @@ import Control.Monad (join)
 import Control.Arrow ((&&&))
 import Data.Maybe (mapMaybe)
 
-data Room = Room { nameChars :: [(Char, Int)]
+data Room = Room { names :: [String]
                  , roomId :: Int
                  , checkSum :: String
                  }
@@ -17,15 +17,14 @@ sortOrder :: (Char, Int) -> (Int, Int)
 sortOrder (c, i) =  (i, negate $ ord c)
 
 valid :: Room -> Bool
-valid r = checkSum r == (take 5 . map fst . nameChars  $ r)
+valid r = checkSum r == (take 5 . map fst) nameChars
+  where nameChars = reverse . sortOn sortOrder . map (head &&& length) . group . sort . join . names $ r
 
-readNameCharsP :: ReadP [(Char, Int)]
-readNameCharsP = do
-  chars <- many1 $ do
-    inner <- many1 $satisfy isLower
-    char '-'
-    return inner
-  return . reverse . sortOn sortOrder . map (head &&& length) . group . sort . join $ chars
+readNameCharsP :: ReadP [String]
+readNameCharsP = many1 $ do
+  inner <- many1 $ satisfy isLower
+  char '-'
+  return inner
 
 readSectorP :: ReadP Int
 readSectorP = read <$> many1 (satisfy isDigit)
@@ -55,4 +54,11 @@ parseInput = mapMaybe roomParser . lines
 aggregate :: [Room] -> Int
 aggregate = sum . map roomId
 
-main = join $ print . aggregate . filter valid . parseInput <$> getContents
+decrypt :: Room -> (String, Int)
+decrypt = inner &&& roomId
+  where
+    shift q = chr . (+ ord 'a') . (`mod` 26) . (+q) . flip (-) (ord 'a') . ord
+    inner (Room names roomId _) = intercalate "-" $ (fmap.fmap) (shift roomId) names
+
+
+main = join $ traverse id . map print . map decrypt . filter valid . parseInput <$> getContents

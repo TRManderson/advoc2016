3c27958164fdadf7984496fd4b31b4873a90a7cc
Tom Manderson <me@trm.io>
2016-12-28 22:09:43 +1000

    Day 18 pt 1

---
+++ src/Day18/Main.hs
@@ -0,0 +1,29 @@
+module Main where
+import Control.Monad (join)
+
+parse ('.':xs) = False : parse xs
+parse ('^':xs) = True : parse xs
+parse _ = []
+
+preds :: [Bool -> Bool -> Bool -> Bool]
+preds =
+  [ \a b c -> a && b && not c
+  , \a b c -> not a && b && c
+  , \a b c -> a && not b && not c
+  , \a b c -> not a && not b && c
+  ]
+
+apply :: Bool -> Bool -> Bool -> (Bool -> Bool -> Bool -> Bool) -> Bool
+apply a b c f = f a b c
+
+nextRow' (a:b:c:xs) = any (apply a b c) preds : nextRow' (b:c:xs)
+nextRow' _ = []
+nextRow xs = nextRow' $ False : (xs ++ [False])
+
+allRows = take 40 . iterate nextRow
+
+pretty = map pretty'
+  where pretty' False = '.'
+        pretty' True  = '^'
+
+main = join $ print . length . filter not . concat . allRows . parse <$> getLine
